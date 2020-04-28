3e9c461b0bf16d6885dfabb5278f08022407ce1b
Nick Pinheiro <nick.pinheiro@microsoft.com>
2020-04-28 03:27:18 -0400

    Models - Added 'Apple' data model.

---
+++ AspNet.Apple/AspNet.Apple.Web.Mvc/Models/Apple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet.Apple.Web.Mvc.Models
+{
+    public class Apple
+    {
+        public class User
+        {
+            public string Id { get; set; }
+            public Name name { get; set; }
+            public string email { get; set; }
+        }
+
+        public class Name
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+        }
+    }
+}
