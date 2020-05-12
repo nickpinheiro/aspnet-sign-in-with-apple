43ae821ffe2c00fc0ae1c85d70ad03923ab9b4b0
Nick Pinheiro <nick.pinheiro@microsoft.com>
2020-05-11 20:04:52 -0400

    MVC - Added App Settings.

--- AspNet.Apple/AspNet.Apple.Web.Mvc/Controllers/HomeController.cs
+++ AspNet.Apple/AspNet.Apple.Web.Mvc/Controllers/HomeController.cs
@@ -6,21 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspNet.Apple.Web.Mvc.Models;
+using Microsoft.Extensions.Options;
 
 namespace AspNet.Apple.Web.Mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppSettings _appSettings;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSettings> appSettings)
         {
             _logger = logger;
+            _appSettings = appSettings.Value;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel()
+            {
+                ClientId = _appSettings.ClientId,
+                RedirectUri = _appSettings.RedirectUri
+            };
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
