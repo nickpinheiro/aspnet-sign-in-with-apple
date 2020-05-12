183dbd37d66d15e26c45887bc03d0deeae0e3b96
Nick Pinheiro <nick.pinheiro@microsoft.com>
2020-05-11 21:54:43 -0400

    Razor - Added App Settings.

--- AspNet.Apple/AspNet.Apple.Web.Razor/Startup.cs
+++ AspNet.Apple/AspNet.Apple.Web.Razor/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNet.Apple.Web.Razor.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,9 @@ namespace AspNet.Apple.Web.Razor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
